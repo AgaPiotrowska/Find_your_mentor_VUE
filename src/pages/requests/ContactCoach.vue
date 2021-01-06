f81d37c944aefe10082698df61c0a8d67b50f819
Aga <agniegrabo@gmail.com>
2021-01-06 17:36:41 +0100

    requests/messages stored in vuex

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -31,6 +31,12 @@
           this.formIsValid = false;
           return;
         }
+        this.$store.dispatch('requests/contactCoach', {
+          email: this.email,
+          message: this.message,
+          coachId: this.$route.params.id
+        });
+        this.$router.replace('/coaches');
       }
     }
   };
